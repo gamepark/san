0d9774f6551fa5966f45c302ec2b1cdd0fb2c978
Romain Fromi <romain.fromi@gmail.com>
2026-09-17 15:47:04 +0200

    collisionAlgorithm={pointerWithin}

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,3 +1,4 @@
+import { pointerWithin } from '@dnd-kit/core'
 import { css } from '@emotion/react'
 import { DevToolsHub, GameTable } from '@gamepark/react-game'
 import { TABLE_HALF_HEIGHT, TABLE_X_MAX, TABLE_X_MIN } from './locators/SanLayout'
@@ -7,7 +8,9 @@ export function GameDisplay() {
   const margin = { top: 7, left: 0, right: 0, bottom: 0 }
   return (
     <>
-      <GameTable xMin={TABLE_X_MIN} xMax={TABLE_X_MAX} yMin={-TABLE_HALF_HEIGHT} yMax={TABLE_HALF_HEIGHT} margin={margin} css={process.env.NODE_ENV === 'development' && tableBorder}>
+      <GameTable xMin={TABLE_X_MIN} xMax={TABLE_X_MAX} yMin={-TABLE_HALF_HEIGHT} yMax={TABLE_HALF_HEIGHT}
+                 collisionAlgorithm={pointerWithin}
+                 margin={margin} css={process.env.NODE_ENV === 'development' && tableBorder}>
         <PlayerPanels />
         {process.env.NODE_ENV === 'development' && <DevToolsHub fabBottom="calc(5em)" />}
       </GameTable>
